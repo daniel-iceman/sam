841ef5b6712c8ab6921ccb3d2359734c22481549
danielHome <md.danieldiaz@gmail.com>
2020-03-11 18:14:06 -0600

    Elaboración de home para pc

--- sam/src/components/Home/HomeDesign.js
+++ sam/src/components/Home/HomeDesign.js
@@ -1,14 +1,23 @@
 //import React, { Component } from 'react'
+//import nurse from '../images/Nurse_short_home_image.jpg'
 import React from 'react';
 import './HomeDesign.css'
-//import nurse from '../images/Nurse_short_home_image.jpg'
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStethoscope } from "@fortawesome/free-solid-svg-icons";
+import { faVials } from "@fortawesome/free-solid-svg-icons";
+import { faRunning } from "@fortawesome/free-solid-svg-icons";
+import { faUserMd } from "@fortawesome/free-solid-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+
 import {Link} from 'react-router-dom'
+import ReactPlayer from 'react-player'
 
 
 function HomeDesign () {
     return (
         <div className='bodyContainer'>
-            <header className='headerPage' >
+            <header className='headerPage' id='headerReturn'>
                     <div className="coverHome">
                         <div className="overlayHome"></div>
                     </div>
@@ -20,14 +29,104 @@ function HomeDesign () {
                             los padecimeintos que requieren atención médica personal, diagnosticar
                             padecimientos y emitir recomendaciones de tratamiento.
                         </p>
-                        <Link to='/registro' >
-                            <button className='buttonCoverHome' type="button">
-                                Registrarme
+                        <div className='botonesHeader' >
+                            <Link to='/registro' >
+                                <button className='buttonSuscribe' type="button">
+                                    Registrarme
+                                </button>
+                            </Link>                            
+                            <div className='buttonConoce' >
+                                <a href='#publicidad'> 
+                                    <button type='button'>Conoce más</button>
+                                </a>
+                                   
+                            </div>
+                        </div>
+                    </div>
+            </header>
+            <section className='contentPage' >
+                <div className='seccion-consultasBotones' >
+                    <div className='tituloConsultas'>
+                        <div className='ayuda-pregunta' >
+                            <h1>¿En que puedo ayudarte?</h1>
+                        </div>
+                    </div>                
+                    <div className='botonesConsultas' >
+                        
+                        <Link to='/sintomas' >
+                            <button className='btn-1'>
+                                <FontAwesomeIcon 
+                                    icon={ faStethoscope } 
+                                    style={{fontSize:'22px', 
+                                            paddingRight:'20px'}}/>
+                                ¿Que molestia tienes?
                             </button>
                         </Link>
                         
+
+                        <Link to='/laboratorios' >
+                            <button className='btn-2'>
+                                <FontAwesomeIcon 
+                                    icon={ faVials } 
+                                    style={{fontSize:'22px', 
+                                            paddingRight:'20px'}}/>                        
+                                ¿Revisamos tus laboratorios?
+                            </button>
+                        </Link>
+
+                        <Link to='/consejos' >
+                            <button className='btn-3'>
+                                <FontAwesomeIcon 
+                                    icon={ faRunning } 
+                                    style={{fontSize:'25px', 
+                                            paddingRight:'20px'}}/>                        
+                                Consejos para tu salud
+                            </button>
+                        </Link>
+
+                        <Link to='/medicos' >
+                            <button className='btn-4'>
+                                <FontAwesomeIcon 
+                                    icon={ faUserMd } 
+                                    style={{fontSize:'25px', 
+                                            paddingRight:'20px'}}/>
+                                Consulta un Médico
+                            </button>
+                        </Link>
                     </div>
-            </header>
+                </div>
+
+                <div className='textoPublicity' id="publicidad" >  
+                    <div className='espacio' ></div>                  
+                    <h1>¿Porqué debo suscribirme?</h1>
+                    <p>En el siguiente video podras ver los beneficios que
+                        puedes obtener al suscribirte en nuestra aplicación.
+                    </p>
+                </div>
+                <div className='videoPublicity' >
+                    <ReactPlayer
+                        url='https://youtu.be/AgBUP8TJqV8'
+                        className='react-player'
+                        playing
+                        width='50%'
+                        height='100%'
+                    />
+                </div>
+
+                <div className='btn-inicio'>
+                    <a href='#headerReturn'> 
+                        <button type='button'>
+                            <FontAwesomeIcon 
+                                icon={ faHome } 
+                                style={{fontSize:'35px', 
+                                paddingRight:'0px',
+                                }}/>
+                            Regresar al inicio
+                        </button>
+                    </a>
+
+                </div>
+            </section>
 
 
         </div>
@@ -61,10 +160,7 @@ class HomeDesign extends Component {
                     </div>
                 </header>
 
-                <div className='contentPage' >
-                    <button>Sintomas</button>
-                    <button>Laboratorios</button>
-                </div>
+                
             </div>
         )
     }
